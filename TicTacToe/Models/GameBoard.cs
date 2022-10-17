24a3604a9641d8c4c65c6f046e8e7b41ad8ce68a
TheWhisperer <houstonplanes@gmail.com>
2022-10-17 20:06:47 +0300

    [Add] GameBoard and GameManager

---
+++ TicTacToe/Models/GameBoard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Models
+{
+    public class GameBoard
+    {
+        public enum Size
+        {
+            Small = 3,
+            Medium = 4,
+            Large = 5,
+        }
+        public enum Slot
+        {
+            Empty,
+            X,
+            O,
+        }
+        public Size BoardSize { get; set; }
+        private readonly Slot[] slots;
+        public Slot[] Slots { get { return slots; } }
+        
+        public GameBoard(Size size)
+        {
+            BoardSize = size;
+            switch (size)
+            {
+                case Size.Small:
+                    slots = new Slot[9];
+                    break;
+                case Size.Medium:
+                    slots = new Slot[16];
+                    break;
+                case Size.Large:
+                    slots = new Slot[25];
+                    break;
+                default:
+                    BoardSize = Size.Small;
+                    slots = new Slot[9];
+                    break;
+            }
+        }
+    }
+}
---
+++ TicTacToe/Models/GameManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Models
+{
+    public class GameManager
+    {
+        public enum GameState
+        {
+            XTurn,
+            OTurn,
+            OWin,
+            XWin,
+            Tie,
+        }
+        public enum GameMode
+        {
+            PvP,
+            PvC,
+            CvC,
+        }
+
+        public GameState State { get; set; } = GameState.XTurn;
+        public GameMode Mode { get; set; } = GameMode.PvP;
+
+        public GameBoard Board { get; }
+        public GameManager(GameBoard.Size size, GameMode mode)
+        {
+            Board = new GameBoard(size);
+            Mode = mode;
+        }
+
+        private static GameManager? instance;
+
+        public static GameManager? Instance
+        {
+            get
+            {
+                return instance;
+            }
+            set
+            {
+                if (instance == null)
+                {
+                    instance = value;
+                }
+            }
+        }
+
+        public bool IsWinning(GameBoard board, GameBoard.Slot player)
+        {
+            bool winning = true;
+            int size = ((int)board.BoardSize);
+
+            // Check for row combinations
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i; j < size + i; j++)
+                {
+                    winning &= board.Slots[j] == player;
+                }
+            }
+            if (winning) return true;
+            winning = true;
+
+            // Check for column combinations
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i; j < size * size; j += size)
+                {
+                    winning &= board.Slots[j] == player;
+                }
+            }
+            if (winning) return true;
+            winning = true;
+
+            // Check for diagonal combinations left to right
+            for (int i = 0; i < size * size; i += size + 1)
+            {
+                winning &= board.Slots[i] == player;
+            }
+            if (winning) return true;
+            winning = true;
+
+            // Check for diagonal combinations right to left
+            for (int i = 0; i < size * size; i += size - 1)
+            {
+                winning &= board.Slots[i] == player;
+            }
+            if (winning) return true;
+            return false;
+
+            // Euristic solution
+
+            //switch (board.BoardSize)
+            //{
+            //    case GameBoard.Size.Small:
+            //        return  (board.Slots[0] == player && board.Slots[1] == player && board.Slots[2] == player) ||
+            //                (board.Slots[3] == player && board.Slots[4] == player && board.Slots[5] == player) ||
+            //                (board.Slots[6] == player && board.Slots[7] == player && board.Slots[8] == player) ||
+            //                (board.Slots[0] == player && board.Slots[3] == player && board.Slots[6] == player) ||
+            //                (board.Slots[1] == player && board.Slots[4] == player && board.Slots[7] == player) ||
+            //                (board.Slots[2] == player && board.Slots[5] == player && board.Slots[8] == player) ||
+            //                (board.Slots[0] == player && board.Slots[4] == player && board.Slots[8] == player) ||
+            //                (board.Slots[2] == player && board.Slots[4] == player && board.Slots[6] == player);
+            //    case GameBoard.Size.Medium:
+            //        return (board.Slots[0] == player && board.Slots[1] == player && board.Slots[2] == player && board.Slots[3])
+            //        break;
+            //    case GameBoard.Size.Large:
+            //        break;
+            //    default:
+            //        break;
+            //}
+        }
+    }
+}
